c5095841be1940259c3987359fca9b26e38bf294
Gustavo273 <gustavo.h.lima@iesb.edu.br>
2025-04-29 19:46:24 -0300

    Prova

--- App.js
+++ App.js
@@ -56,7 +56,7 @@ export default function App() {
             title: "Brasil",
             headerTitleAlign: "center",
             headerStyle: {
-              backgroundColor: "white",
+              backgroundColor: "black",
             },
             tabBarInactiveBackgroundColor: "white",
             tabBarIcon: ({ color, size }) => (
